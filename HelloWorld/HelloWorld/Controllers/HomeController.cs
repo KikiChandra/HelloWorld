044a2da216756f635e6c2e17a53df11b5c0d111e
Kiki Chandra <ki2chandra@gmail.com>
2012-07-30 17:24:38 +1000

    Update HelloWorld/HelloWorld/Controllers/HomeController.cs

--- HelloWorld/HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace HelloWorld.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Cmon !!!!";
 
             return View();
         }
